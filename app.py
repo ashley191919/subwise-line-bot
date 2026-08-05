3ac9c4f844fe32ccd1c39930f04e61c589b9fa2e
ashley <413570115@cloud.fju.edu.tw>
2026-08-05 16:08:17 +0800

    Day 04: Add LINE webhook integration

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
+from dotenv import load_dotenv
 
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+
+load_dotenv()
 app = Flask(__name__)
+configuration = Configuration(
+    access_token=os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+)
+
+handler = WebhookHandler(
+    os.getenv("LINE_CHANNEL_SECRET")
+)
 
 @app.route("/")
 def home():
@@ -9,6 +21,18 @@ def home():
         "status": "success",
         "message": "SubWise Backend is running!"
     })
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers.get("X-Line-Signature")
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return "Invalid signature", 400
+
+    return "OK"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
