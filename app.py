a98f6e73b297be6f5cd4b4a19287cd2b4224d9a3
ashley <413570115@cloud.fju.edu.tw>
2026-08-06 16:02:11 +0800

    Day 05: Implement LINE Echo Bot

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ import os
 from dotenv import load_dotenv
 
 from linebot.v3 import WebhookHandler
-from linebot.v3.messaging import Configuration
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
 from linebot.v3.exceptions import InvalidSignatureError
 
 load_dotenv()
@@ -34,6 +41,24 @@ def callback():
         return "Invalid signature", 400
 
     return "OK"
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+
+    with ApiClient(configuration) as api_client:
+
+        line_bot_api = MessagingApi(api_client)
+
+        line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    TextMessage(
+                        text=f"你剛剛說：{event.message.text}"
+                    )
+                ]
+            )
+        )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
